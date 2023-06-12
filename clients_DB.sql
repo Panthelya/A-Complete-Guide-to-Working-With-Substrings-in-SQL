2b027d8ce35f9ed127372b98bb458811fe5009c5
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-06-12 17:04:56 +0200

    Update clients_DB.sql

--- clients_DB.sql
+++ clients_DB.sql
@@ -2,14 +2,13 @@ CREATE TABLE clients (
 	id INT,
 	first_name VARCHAR(50),
 	last_name VARCHAR(50),
-	date_of_birth DATE,
-	text_date_of_birth VARCHAR(50),
+	date_of_birth VARCHAR(50),
 	email VARCHAR(50)
 	
 );
 
-INSERT INTO clients (id, first_name, last_name, date_of_birth, text_date_of_birth, email) VALUES (1, 'Mr. Isaac', 'Guardiola', '1994-08-19', '19.08.1994', 'iguardiola@gmail.com');
-INSERT INTO clients (id, first_name, last_name, date_of_birth, text_date_of_birth, email) VALUES (2, 'Mx. Carry', 'Jones', '1982-06-19', '19.06.1982', 'cjones@yahoo.com');
-INSERT INTO clients (id, first_name, last_name, date_of_birth, text_date_of_birth, email) VALUES (3, 'Mr. Frank', 'Thomas', '1954-01-01', '01.01.1994', 'fthomas@yahoo.com');
-INSERT INTO clients (id, first_name, last_name, date_of_birth, text_date_of_birth, email) VALUES (4, 'Ms. Marianne', 'Meijer', '1989-11-27', '27.11.1989', 'mmeijer@meijer.com');
-INSERT INTO clients (id, first_name, last_name, date_of_birth, text_date_of_birth, email) VALUES (5, 'Mr. Dave', 'Trotter', '1957-04-15', '15.04.1957', 'dtrotter@aol.com');
+INSERT INTO clients (id, first_name, last_name, date_of_birth, email) VALUES (1, 'Mr. Isaac', 'Guardiola', '19.08.1994', 'iguardiola@gmail.com');
+INSERT INTO clients (id, first_name, last_name, date_of_birth, email) VALUES (2, 'Mx. Carry', 'Jones', '19.06.1982', 'cjones@yahoo.com');
+INSERT INTO clients (id, first_name, last_name, date_of_birth, email) VALUES (3, 'Mr. Frank', 'Thomas', '01.01.1994', 'fthomas@yahoo.com');
+INSERT INTO clients (id, first_name, last_name, date_of_birth, email) VALUES (4, 'Ms. Marianne', 'Meijer', '27.11.1989', 'mmeijer@meijer.com');
+INSERT INTO clients (id, first_name, last_name, date_of_birth, email) VALUES (5, 'Mr. Dave', 'Trotter', '15.04.1957', 'dtrotter@aol.com');
